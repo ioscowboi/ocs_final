bdfc8382c6dec703b56a208ba0a482241609e219
CharDale Barnes <differentaccounts@hotmail.com>
2015-03-25 09:27:24 -0500

    Pretty much finished. Just need to add some meaningful text and icons to add a more professional look. Works best in Chrome browser. Need to add some responsive design and refactor for other browsers.

--- app/assets/javascripts/parallax.js
+++ app/assets/javascripts/parallax.js
@@ -4,10 +4,14 @@ function parallax(){
   var heartbeater = document.getElementById('heartbeater');
   var heartbeater2 = document.getElementById('heartbeater2');
   var heartbeater3 = document.getElementById('heartbeater3');
-  heartbeat.style.top = -(window.pageYOffset * 2)+'px';
+  var title = document.getElementById('title');
+  var section = document.getElementById('cont2');
+  heartbeat.style.top = -(window.pageYOffset * 3.5)+'px';
   heartbeat2.style.top = -(window.pageYOffset / 8)+'px';
   heartbeater.style.top = -(window.pageYOffset / 2)+'px';
   heartbeater2.style.top = (window.pageYOffset / 2)+'px';
   heartbeater3.style.top = -(window.pageYOffset / 10)+'px';
+  title.style.top = -(window.pageYOffset)+'px';
+  section.style.top = -(window.pageYOffset / 3)+'px';
 }
 window.addEventListener("scroll", parallax, false);
